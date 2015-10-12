2b44bbea5c0164782be0274da910231fd718fcfb
unknown <daumisgod@gmail.com>
2015-10-12 14:49:02 -0400

    fixed boot bugs

--- LanguageGame/www/Boot.js
+++ LanguageGame/www/Boot.js
@@ -1,12 +1,14 @@
 /**
  * Created by mdaum on 10/12/2015.
  */
-var LanguageGame{};
-LanguageGame.Boot.prototype{
-  preload:function(){
-    
+var LanguageGame = {};
+LanguageGame.Boot=function(game){};
+LanguageGame.Boot.prototype={
+  preload: function(){
+    this.load.image('preloadBar','assets/images/loading.png');
+
   },
-    create:function(){
+    create: function(){
 
     }
 };
