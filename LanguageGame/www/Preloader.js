602c2515fe91b92fb5e3356fc182a988b96040b5
tcntounc <tcntounc@live.unc.edu>
2015-10-12 15:14:07 -0400

    Changed Preloader.js

--- LanguageGame/www/Preloader.js
+++ LanguageGame/www/Preloader.js
@@ -1,10 +1,17 @@
 LanugageGame.Preloader = function(game) {
+    this.preloadBar = null;
     this.ready = false;
 };
 
 LanguageGame.Preloader.prototype = {
 
     preload: function() {
-        this.load.bitmapFont
+        this.preloadBar = this.add.sprite(this.world.centerX, this.world.centerY, 'preloadBar');
+        this.preloadBar.anchor.setTo(0.5,0.5);
+        this.load.bitmapFont('eightbitwonder', 'assets/fonts/eightbitwonder.png', 'assets/fonts/eightbitwonder.fnt');
+    },
+
+    create : function() {
+        this.preloadBar.cropEnabled = false;
     }
 }
