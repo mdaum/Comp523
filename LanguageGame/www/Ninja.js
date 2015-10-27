de05754b088aeae44f9c60a6c17f0b8fab668e0d
unknown <daumisgod@gmail.com>
2015-10-26 22:03:56 -0400

    updated phaser...fixes factorial method not found issue

--- LanguageGame/www/Ninja.js
+++ LanguageGame/www/Ninja.js
@@ -1,11 +1,24 @@
 LanguageGame.Ninja = function(game) {
-    this.ninjaText;
+    this.points;
 };
 
 LanguageGame.Ninja.prototype = {
     create: function() {
-        ninjaText = this.add.bitmapText(this.world.centerX - 155, this.world.centerY + 180, 'eightbitwonder', 'Click on me to do Database stuff!', 24);
-
+        var square = this.add.graphics();
+        square.beginFill(0xffffff);
+        square.drawCircle(0, 0, 20);
+        square.position.set(4,484);
+        var tween = this.add.tween(square).to({
+            x: [4,85,136,152,238,322],
+            y: [483,22,273,22,483,40]
+        }, 2000);
+        tween.interpolation(function(v, k){
+            return Phaser.Math.bezierInterpolation(v, k);
+        });
+        tween.repeat(Infinity);
+        tween.start();
     }
+
 };
+
 
