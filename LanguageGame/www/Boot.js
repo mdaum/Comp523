e5bce22bc425d620ea27ab8370d85b6df02ecce5
Spencer Byers <sgbyers@live.unc.edu>
2015-11-01 22:03:05 -0500

    test

--- LanguageGame/www/Boot.js
+++ LanguageGame/www/Boot.js
@@ -12,15 +12,47 @@ LanguageGame.Boot.prototype={
     create: function(){
         this.input.maxPointers = 1;
         this.stage.disableVisibilityChange = false;
-        this.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-        this.scale.minWidth = 270;
-        this.scale.minHeight = 480;
-        this.scale.pageAlignHorizontally = true;
-        this.scale.pageAlignVertically = true;
-        this.stage.forcePortrait = true;
-       // this.scale.setScreenSize(true); //this is HUGE problem!!
+        this.scaleStage();
         this.input.addPointer();
         this.stage.backgroundColor = '#171642';
         this.state.start('Preloader');
+    },
+
+    scaleStage: function(){
+
+        //this.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+        //this.scale.minWidth = 270;
+        //this.scale.minHeight = 480;
+        //this.scale.pageAlignHorizontally = true;
+        // this.scale.pageAlignVertically = true;
+        //this.stage.forcePortrait = true;
+        // this.scale.setScreenSize(true); //this is HUGE problem!!
+
+        if (this.game.device.desktop)
+        {
+            this.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+            this.scale.minWidth = this.width/2;
+            this.scale.minHeight = this.height/2;
+            this.scale.maxWidth = this.width;
+            this.scale.maxHeight = this.height;
+            this.scale.pageAlignHorizontally = true;
+            this.scale.pageAlignVertically = true;
+           // this.scale.setScreenSize(true);
+        }
+        else
+        {
+            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.scale.minWidth = this.width/2;
+            this.scale.minHeight = this.height/2;
+            this.scale.maxWidth = 2048; //You can change this to gameWidth*2.5 if needed
+            this.scale.maxHeight = 1228; //Make sure these values are proportional to the gameWidth and gameHeight
+            this.scale.pageAlignHorizontally = true;
+            this.scale.pageAlignVertically = true;
+            this.scale.forceOrientation(true, false);
+            //this.scale.hasResized.add(this.gameResized, this);
+            this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
+            this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
+            //this.scale.setScreenSize(true);
+        }
     }
 };
