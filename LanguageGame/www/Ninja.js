bc347925178a81b3447eb63dd46e5773ac69efe6
Spencer Byers <sgbyers@live.unc.edu>
2015-11-23 21:34:25 -0500

    Ninja logic

--- LanguageGame/www/Ninja.js
+++ LanguageGame/www/Ninja.js
@@ -4,55 +4,64 @@ LanguageGame.Ninja = function (game) {
     this.ninjaBG = null;
     this.wordBox = null;
     this.backBox = null;
+    this.livesBox = null;
     this.backText = "";
     this.backWordText = "";
-    //this.boxWordText;
     this.boxText = "";
     this.numCards = 0;
     this.goodCard = 0;
     this.results = null;
     this.cardArray = [];
+    this.category = "";
+    this.lives = 0;
 };
 
 LanguageGame.Ninja.prototype = {
     create: function () {
+        var style = {font: "50px Georgia", fill: "000000", align: "center"};
 
-        this.numCards = 0;
+        //------- select the category for the round ------------//
+        this.results = LanguageGame.gameDB.exec("SELECT DISTINCT category FROM test");
+        var len = this.results[0]["values"].length;
+        var rnd = Math.floor(Math.random()*len);
+        this.category = this.results[0]["values"][rnd][0];
+        console.log(len);
+        console.log(rnd);
+        //---------------------------------------------//
 
-        this.results = LanguageGame.gameDB.exec("SELECT english, unicode FROM test T WHERE T.category like '%number%'");
-        var randNumber = Math.floor(Math.random() * 9);
-        var boxWordText = this.results[0]["values"][randNumber][0];
-        var unicodeVal = this.results[0]["values"][randNumber][1];
-
+        //------------- initialize game state ------------//
+        this.results = LanguageGame.gameDB.exec("SELECT english, unicode FROM test T WHERE T.category like '%"+ this.category+"%'");
         this.ninjaBG = this.add.image(this.world.centerX - 270, this.world.centerY - 480, 'dojo');
         this.wordBox = this.add.image(this.world.centerX - 100, this.game.height - 60, 'box');
-        this.backBox = this.add.image(0, this.game.height - 60, 'box');
         this.card = this.add.image(-400, -400, 'card');
         this.card.goodCard = "false";
-        // this.boxWordText = "Blue";
-        this.backWordText = "Back";
-        var style = {font: "50px Georgia", fill: "000000", align: "center"};
-        this.boxText = this.game.add.text(this.wordBox.width / 2, this.card.height / 8 + 10, boxWordText, style);
-        this.backText = this.game.add.text(this.backBox.width / 2, this.backBox.height / 2 - 35, this.backWordText, style);
-        this.boxText.anchor.set(0.5);
-        this.backText.anchor.set(0.5);
-        this.wordBox.addChild(this.boxText);
-        this.backBox.addChild(this.backText);
+        //------------------------------------------------------//
 
+        //--------------------- Create Back Button -----------------//
+        this.backBox = this.add.image(0, this.game.height - 60, 'box');
+        this.backWordText = "Back";
+        this.backText = this.game.add.text(this.backBox.width / 2, this.backBox.height / 2 - 35, this.backWordText, style);
+        this.backText.anchor.set(0.5);
+        this.backBox.addChild(this.backText);
         this.backBox.inputEnabled = true; //now we can accept clicks/touches
         this.backBox.events.onInputDown.addOnce(this.back, this); //will happen when input happens
+        //------------ end back button ---------------------------//
 
+        //------------- count for player lives to be displayed -----//
+        this.lives = 3;
+        this.livesBox = this.add.image(this.world.width - 200, 10, 'box');
+        var lifeText = this.game.add.text(this.livesBox.width / 2, this.livesBox.height / 2 - 35, this.lives, style);
+        lifeText.anchor.set(0.5);
+        this.livesBox.addChild(lifeText);
 
-        //------------I HAVE NO IDEA WHAT IM DOING - S -----------//
-        this.newCard(this, unicodeVal);
+        //---------------------------------------------//
 
-        for (var i = 0; i < 2; i++) {
-            randNumber = Math.floor(Math.random() * 9);
-            unicodeVal = this.results[0]["values"][randNumber][1];
-            this.newCard(this, unicodeVal);
+        //------------ Make Starting Cards -----------//
+        for (var i = 0; i < 3; i++) {
+            this.aNewCard();
         }
+        console.log(this.numCards);
         //------------------------------//
-        this.update();
     },
 
     stopCard: function (pointer, card, tween, bool) {
@@ -60,76 +69,37 @@ LanguageGame.Ninja.prototype = {
         //var c = child.parent;
         console.log(bool);
         if (bool == "true") {
-            this.goodCard--;
+            this.lives++;
+            //increment score
+        }else{
+            this.lives--;
+            //portray decremented lives and check for death
         }
         tween.stop(true);
-        this.numCards--;
-        this.aNewCard(this);
 
-    },
-
-    newCard: function (game, unicodeVal) {
-
-        this.numCards++;
-        var style = {font: "50px Georgia", fill: "000000", align: "center"};
-
-        var card = this.add.image(0, this.game.height, 'card');
-        card.goodCard = "false";
-        //--------------------------------------------//
-        if (this.goodCard < 1) {
-            card.goodCard = "true";
-            this.goodCard++;
+        if(this.numCards==0) {
+            for (var i = 0; i < 3; i++) {
+                this.aNewCard();
+            }
         }
-        //--------------------------------------------//
-        var cardText = this.game.add.text(card.width / 2, card.height / 2, String.fromCharCode(unicodeVal), style);
-        cardText.anchor.set(0.5);
-        card.addChild(cardText);
 
-        // console.log(card.goodCard);
-
-        var x1 = 4;
-        var x2 = 100;
-        var x3 = 136;
-        var x4 = 300;
-        var x5 = 438;
-        var x6 = 500;
-
-        var tween = this.add.tween(card).to({
-            x: [x1, x2, x3, x4, x5, x6],
-            y: [483, 0, 50, 0, 383, 900]
-        }, 5000);
-
-        tween.interpolation(function (v, k) {
-            return Phaser.Math.bezierInterpolation(v, k);
-        });
-
-        var tween2 = this.game.add.tween(card).to({x: card.x, y: this.game.height + 500}, 1);
-        tween.chain(tween2);
-
-        card.inputEnabled = true;
-        cardText.inputEnabled = true;
-        card.events.onInputDown.addOnce(this.stopCard, this, this.card, tween, card.goodCard); //will happen when input happens
-        cardText.events.onInputDown.addOnce(this.stopCard, this, this.card, tween); //will happen when input happens
-
-        tween.delay(1000 * this.numCards);
-        tween.start();
-
-        this.cardArray[this.numCards] = card.goodCard;
     },
 
     aNewCard: function (game) {
         this.numCards++;
-        var randNumber = Math.floor(Math.random() * 9);
+
+        var catLen = this.results[0]["values"].length;
+        var randNumber = Math.floor(Math.random() * catLen);
         var boxWordText = this.results[0]["values"][randNumber][0];
         var unicodeVal = this.results[0]["values"][randNumber][1];
 
-
+        var cardStyle = {font: "60px Serif", fill: "000000", align: "center"};
         var card = this.add.image(0, this.game.height, 'card');
-        var cardText = this.game.add.text(card.width / 2, card.height / 2, String.fromCharCode(unicodeVal), style);
+        var cardText = this.game.add.text(card.width / 2, card.height / 2, String.fromCharCode(unicodeVal), cardStyle);
         cardText.anchor.set(0.5);
         card.addChild(cardText);
-
         card.goodCard = "false";
+
         //--------------------------------------------//
         if (this.goodCard < 1) {
             card.goodCard = "true";
@@ -138,10 +108,11 @@ LanguageGame.Ninja.prototype = {
         //--------------------------------------------//
 
         var style = {font: "50px Georgia", fill: "000000", align: "center"};
-        var newBoxText = this.game.add.text(this.wordBox.width / 2, this.card.height / 8 + 10, boxWordText, style);
+
 
         if (card.goodCard == "true") {
 
+            var newBoxText = this.game.add.text(this.wordBox.width / 2, this.card.height / 8 + 10, boxWordText, style);
             newBoxText.anchor.set(0.5);
             this.wordBox.removeChild(this.boxText);
             this.wordBox.addChild(newBoxText);
@@ -171,26 +142,66 @@ LanguageGame.Ninja.prototype = {
         card.inputEnabled = true;
         cardText.inputEnabled = true;
         card.events.onInputDown.addOnce(this.stopCard, this, this.card, tween, card.goodCard); //will happen when input happens
-        cardText.events.onInputDown.addOnce(this.stopCard, this, this.card, tween); //will happen when input happens
 
         tween.delay(1000 * this.numCards);
         tween.start();
 
-        this.cardArray[this.numCards] = card.goodCard;
+        this.cardArray[this.numCards-1] = card;
+    },
+
+    clear: function(){
+        this.points = null;
+        this.card = null;
+        this.ninjaBG = null;
+        this.wordBox = null;
+        this.backBox = null;
+        this.backText = "";
+        this.backWordText = "";
+        this.boxText = "";
+        this.numCards = 0;
+        this.goodCard = 0;
+        this.results = null;
+        this.cardArray = [];
+        this.category = "";
+        this.lives = 0;
     },
 
     back: function (pointer) {
+        this.clear();
         this.state.start('MainMenu');
     },
 
     update : function(){
-        var trueCard = "truth has been found";
-        if(this.cardArray[this.numCards]== "true"){
-            console.log(trueCard);
+        for(var i = 0; i < this.cardArray.length;i++)
+        {
+            if(this.cardArray[i] != null && ( this.cardArray[i].x > this.game.width || this.cardArray[i].y > this.game.height))
+            {
+
+                if (this.cardArray[i].goodCard == "true") {
+                    this.goodCard--;
+                    this.lives--;
+                }
+
+                this.cardArray[i].kill();
+                this.cardArray[i] = null;
+                this.numCards--;
+                console.log(this.numCards);
+            }
         }
 
-    }
+        if(this.numCards == 0)
+        {
+            for (var i = 0; i < 3; i++) {
+                this.aNewCard();
+            }
+        }
 
+    if(this.lives <=0) {
+        alert("GAME OVER");
+        this.clear();
+        this.create();
+    }
+    }
 
 };
 
